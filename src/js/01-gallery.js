302c65e4750c2d5d98e33811770c5b24462531ec
Albina Vivdenko <albinka3322@gmail.com>
2023-11-19 17:31:53 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const ulEl = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({ original, preview, description }) =>
+            `<li class= "gallery__item" >
+        <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a></li>`
+        ).join('');
+
+};
+
+ulEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+
+const lightbox = new SimpleLightbox('.gallery-items.js a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
