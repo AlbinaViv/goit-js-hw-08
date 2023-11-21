61da4df1af9b4d3522af366dfe366e39a4d7f074
Albina Vivdenko <albinka3322@gmail.com>
2023-11-21 15:51:11 +0100

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,91 +1,42 @@
 import throttle from 'lodash.throttle';
 
 import '../css/common.css';
-// const LS_KEY = "feedback-form-state";
-// const refs = {
-//     form: document.querySelector(".feedback-form"),
-//     textarea: document.querySelector("textarea"),
-// };
+const LS_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+// const email = form.elements.email;
+// const message = form.elements.message;
+const valueEl = localStorage.getItem(LS_KEY);
+
+document.addEventListener('DOMContentLoaded', onFormValue);
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(savedFormData, 500));
+
 // const formData = {};
 
-
-// refs.form.addEventListener("submit", onFormSubmit);
-// refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
-
-// refs.form.addEventListener("input", event => {
-//     console.log(event.target.name);
-//     console.log(event.target.value);
-
-//     formData[event.target.name] = event.target.value;
-
-//     console.log(formData);
-
-//     localStorage.setItem(LS_KEY, JSON.stringify(formData));
-
-//     const value = localStorage.getItem(LS_KEY);
-//     console.log(value);
-//     console.log(JSON.parse(value));
-
-// });
-
-
-// populateTextarea();
-
-// function onFormSubmit(event) {
-//     event.preventDefault();
-//     console.log("відправка форми");
-
-//     event.currentTarget.reset();
-//     localStorage.removeItem(LS_KEY);
-// }
-
-// function onTextareaInput(event) {
-//     const message = event.target.value;
-//     localStorage.setItem(LS_KEY, message);
-
-// }
-// function populateTextarea() {
-//     const savedTextarea = localStorage.getItem(LS_KEY);
-//     if (savedTextarea) {
-//         refs.textarea.value = savedTextarea;
-//     }
-// } 
-
-import throttle from "lodash.throttle";
-const form = document.querySelector('.feedback-form');
-const emailInput = form.elements.email;
-const messageInput = form.elements.message;
-
-const formFields = () => {
-    const jsonString = localStorage.getItem('feedback-form-state');
-    if (jsonString) {
-        const formData = JSON.parse(jsonString);
+const onFormValue = () => {
+    if (valueEl) {
+        const formData = JSON.parse(valueEl);
         emailInput.value = formData.email;
         messageInput.value = formData.message;
     }
 };
-document.addEventListener('DOMContentLoaded', formFields);
 
-const handleSubmit = event => {
+const onFormSubmit = event => {
     event.preventDefault();
-    const jsnStr = localStorage.getItem('feedback-form-state');
-    if (jsnStr) {
-        const formData = JSON.parse(jsnStr);
+    const valueEl = localStorage.getItem('feedback-form-state');
+    if (valueEl) {
+        const formData = JSON.parse(valueEl);
         console.log(formData);
         localStorage.removeItem('feedback-form-state');
         form.reset();
     }
 };
-form.addEventListener('submit', handleSubmit);
 
 const savedFormData = () => {
     const formData = {
         email: emailInput.value,
         message: messageInput.value,
     };
-    const jsonStringThrottle = JSON.stringify(formData);
-    localStorage.setItem('feedback-form-state', jsonStringThrottle);
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
-const throttledSaveFormData = throttle(savedFormData, 500);
-form.addEventListener('input', throttledSaveFormData);
