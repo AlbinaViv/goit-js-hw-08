d0ff37f9b42689661e5ea16366f00fef6e4ed226
Albina Vivdenko <albinka3322@gmail.com>
2023-11-25 13:34:02 +0100

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,11 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
 
 
 const onPlay = function (data) {
